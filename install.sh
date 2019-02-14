cda5160f987f49081a577c504a9e53b0bfc083ed
Anton Lindström <lindstrom.anton@gmail.com>
2019-02-14 19:43:55 +0100

    removed plug.vim

--- install.sh
+++ install.sh
@@ -6,11 +6,8 @@ cp .tmux.conf ~/
 
 if [ ! -f ./plug.vim ] ; then
     curl -o plug.vim https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
 fi
 
 mkdir -p ~/.vim/autoload
-
-cp plug.vim ~/.vim/autoload/
-
+mv plug.vim ~/.vim/autoload/
 mkdir -p ~/.vim/plugged
